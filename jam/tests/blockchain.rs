4147ba572ffc6d66b8a927502930e2bb35f751ec
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2024-06-26 13:00:12 +0200

    blockchain tests

---
+++ jam/tests/blockchain.rs
@@ -0,0 +1,40 @@
+#[cfg(test)]
+mod tests {
+    use jam::blockchain::Blockchain;
+    use jam::transaction::Transaction;
+
+    #[test]
+    fn test_create_genesis_block() {
+        let blockchain = Blockchain::new(2);
+        assert_eq!(blockchain.chain.len(), 1);
+        assert_eq!(blockchain.chain[0].index, 0);
+    }
+
+    #[test]
+    fn test_add_transaction() {
+        let mut blockchain = Blockchain::new(2);
+        let transaction = Transaction::new("Alice".to_string(), "Bob".to_string(), 10.0, 1);
+        blockchain.add_transaction(transaction.clone());
+        assert_eq!(blockchain.pending_transactions.len(), 1);
+        // TODO fix
+        // assert_eq!(blockchain.pending_transactions[0], transaction);
+    }
+
+    #[test]
+    fn test_mine_pending_transactions() {
+        let mut blockchain = Blockchain::new(2);
+        blockchain.add_transaction(Transaction::new("Alice".to_string(), "Bob".to_string(), 10.0, 1));
+        blockchain.mine_pending_transactions("Miner1".to_string());
+        assert_eq!(blockchain.chain.len(), 2);
+        assert_eq!(blockchain.pending_transactions.len(), 1);
+        assert_eq!(blockchain.pending_transactions[0].receiver, "Miner1");
+    }
+
+    #[test]
+    fn test_is_chain_valid() {
+        let mut blockchain = Blockchain::new(2);
+        blockchain.add_transaction(Transaction::new("Alice".to_string(), "Bob".to_string(), 10.0, 1));
+        blockchain.mine_pending_transactions("Miner1".to_string());
+        assert!(blockchain.is_chain_valid());
+    }
+}
